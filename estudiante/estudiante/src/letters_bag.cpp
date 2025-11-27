3e1dd930bb094b4035aab6daa2516e105b722d1c
a-a-ro-n <aaronlopmol@gmail.com>
2025-11-27 23:41:09 +0100

    implementacion del constructor, getter y toString()

--- estudiante/estudiante/src/letters_bag.cpp
+++ estudiante/estudiante/src/letters_bag.cpp
@@ -1,9 +1,29 @@
 #include "letters_bag.h"
 
-LettersBag(const LettersSet conjunto){  // pasamos el numero de letras, y el conjunto dnde estan las letras
+LettersBag::LettersBag(const LettersSet & conjunto){    // pasamos el numero de letras, y el conjunto dnde estan las letras
+    auto it = conjunto.begin();         // iterador a la primera posicon del conjunto
     
+    for(it; it != conjunto.end(); it++){
+        int reps = it->second.repetitions;  // repeticiones
+        char letra = it->first;             // letra
+
+        for(int i = 0; i < reps; i++)   // añadimos la letra tantas veces como repetciones tenga
+            bag.push_back(letra);
+    }
 }
 
-const char getLetter() const;                   // conseguimos la letra
-// void setLetter(const char);                  // añadimos la letra a la bolsa   (no le veo mucho sentido que podamos añadir letras)
-const string toString();                        // mostramos por pantalla todas las letras de la bolsa
+const char LettersBag::getLetter(int index) const {     // conseguimos la letra
+    char result = (index > -1 && index < bag.size()) ? bag[index] : '\0';
+    return result;
+}
+
+// void setLetter(const char);          // añadimos la letra a la bolsa   (no le veo mucho sentido que podamos añadir letras)
+
+const string LettersBag::toString() {   // mostramos por pantalla todas las letras de la bolsa
+    string result = "{";
+    for(int i = 0; i < bag.size()-1; i++)
+       result +=  bag[i] + ",";
+    result += bag[bag.size()-1] + "}";
+
+    return result;
+}
