e948b7b6c3a0a11c45a1f8050e4f003776937783
a-a-ro-n <aaronlopmol@gmail.com>
2025-12-01 11:52:34 +0100

    ajuste en el modo de juego, separandolo por funiones fuera del main

--- estudiante/estudiante/src/letras.cpp
+++ estudiante/estudiante/src/letras.cpp
@@ -2,25 +2,27 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <time.h>
 
 #include "dictionary.h"
 #include "letters_bag.h"
 #include "letters_set.h"
 #include "solver.h"
 
+
 using namespace std;
 
 void ModoPalabraMasLarga(LettersSet &, const int, const LettersBag &, string &, set<string> &, string &);
 void ModoPalabraMayorPuntuacion(LettersSet &, const int, const LettersBag &, string &, set<string> &, string &);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     if(argc != 6)
         return 1; // si no tenemos todos los parametros de entrada salimos del programa
     
     LettersSet fichero(argv[4].toString());     // obtenemos las letras del archivo pasado
-    const int CANTIDAD_LETRAS = argv[5];        // obtenemos la cantidad de laetras a tener
+    const int CANTIDAD_LETRAS = argv[5].stoi(); // obtenemos la cantidad de laetras a tener
     const LettersBag letras(CANTIDAD_LETRAS);   // contenedor de letras para formar la palabra
+
     string solucion_usuario = "";               // palabra digitada por el usuario
     set<string> soluciones;                     // contenedor de resultados
     string mejor_solucion = "";                 // almaceno la mejor solucion
@@ -39,26 +41,39 @@ int main(int argc, char *argv[])
 }
 
 
-void ModoPalabraMasLarga(LettersSet &, const int, const LettersBag &, string &, set<string> &, string &)
-{
+void ModoPalabraMasLarga(LettersSet & fichero, const int CANTIDAD_LETRAS, const LettersBag & letras, string & solucion_usuario, set<string> & soluciones, string & mejor_solucion){
 
 }
 
-void ModoPalabraMayorPuntuacion(LettersSet &, const int, const LettersBag &, string &, set<string> &, string &)
-{
+void ModoPalabraMayorPuntuacion(LettersSet & fichero, const int CANTIDAD_LETRAS, const LettersBag & letras, string & solucion_usuario, set<string> & soluciones, string & mejor_solucion){
+    srand(time(NULL));                              // obtenemos una semilla en este mismo intante
     int puntuacion_palabra = 0;
     bool seguir_jugando = true;
 
     while ( seguir_jugando ){
         cout << "Las letras son: ";
         string cadena = "";
-        for( int i = 0; i < CANTIDAD_LETRAS; i++ )
-            cadena += "\t" + letras[i];
+
+        multimap<int,char> posicion_letra[CANTIDAD_LETRAS];      // map de la posicion de las letras para manejarlas como un array
+        multimap<char,int> puntuacion_letra[CANTIDAD_LETRAS];    // map de puntuacion las letras, dadas una letra
+
+        for( int i = 0; i < CANTIDAD_LETRAS; i++ ){
+            char c = letras[rand() % letras.size()];    // obtenemos la letra
+            int puntuacion = fichero.get[c].score;      // obtenemos la puntuacion de la letra
+
+            posicion_letra[i] = c;                      // añadimos en la posicion i la letra aleatoria del conjunto
+            puntuacion_letra[c] = puntuacion;           // añadimos la puntuacion dada la letra obtenida;
+
+            cadena += "\t" + posicion_letra[i];
+        }
         cout << cadena << endl;                     // sacamos por pantalla las letras
 
         cout << "Dime tu solucion: " ;
         cin >> solucion_usuario;                    // Recogemos la solucion apriori del usuario
         cout << solucion_usuario << endl;           // Imprimimos la solucion
+        /*
+            HAY QUE COMPROBAR SI LA PALABRA EXISTE SIENDO VALIDA 
+        */
         cout << solucion_usuario << "\tPuntuación: "
                << puntuacion_palabra << endl;       // Mostramos la puntuacion de la palabra
 
@@ -69,8 +84,7 @@ void ModoPalabraMayorPuntuacion(LettersSet &, const int, const LettersBag &, str
         cout << "Mejor Solucion: " << mejor_solucion << endl;
         
         char result = '\0';
-        do
-        {
+        do{
             cout << "¿Quieres seguir jugando [S/N]?";
             char aux;
             cin >> aux;
@@ -78,8 +92,7 @@ void ModoPalabraMayorPuntuacion(LettersSet &, const int, const LettersBag &, str
                 result = 's';
             else if ( aux == 'N' || aux == 'n')
                 result = 'n';
-        }
-        while ( result == '\0' ) // mientras que no haya resultado, no se hace nada
+        }while ( result == '\0') // mientras que no haya resultado, no se hace nada
 
         seguir_jugando = (result == 's') ? true : false;
     }
