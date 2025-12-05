16f491302de9d6a4f1028c970167ff79b4024001
pablo <pablo@FamiliaBM>
2025-12-05 21:24:14 +0100

    He hecho solver. Para getSolutions he seguido lo de las diapositivas. He hecho una función auxiliar para saber si una palabra (pasada del diccionario) se puede construir con el conjunto de letras que tenemos

--- estudiante/estudiante/src/solver.cpp
+++ estudiante/estudiante/src/solver.cpp
@@ -1,3 +1,43 @@
 #include "solver.h"
 //A completar
 
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "letters_set.h"
+#include "dictionary.h"
+
+using namespace std;
+
+bool Solver::poderConstruir(string palabra, vector<char> available_letters){
+    bool esta = true;
+    for(int i = 0; i < palabra.size() && esta; ++i){
+        esta = false;
+        for(auto it = available_letters.begin(); it != available_letters.end(); ++it){
+            if(palabra[i] == *it){
+                esta = true;
+                auto it2 = it;
+                it = available_letters.erase(it2);
+            }
+        }
+    }
+    return esta;
+}
+
+vector<string> Solver::getSolutions(const vector<char>& available_letters, bool score_game){
+    vector<string> out;
+    Dictionary dict;
+    for(auto it = dict.begin(); it != dict.end(); ++it){
+        if(poderConstruir(*it, available_letters) ){
+            if(*it.size() > out[0].size() ){    //comparo solo con el primer elemento, ya que si hay más, todos tendrán su mismo tamaño
+                out.clear();
+                out.push_back(*it);
+            }else if(*it.size() == out[0].size() ){
+                out.push_back(*it);
+            }
+        }
+    }
+return out;
+}
+
