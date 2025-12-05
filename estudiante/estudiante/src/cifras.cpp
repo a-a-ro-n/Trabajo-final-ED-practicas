026fc48d2e8301d35a0ffa88d9dbb58ded588938
pablo <pablo@FamiliaBM>
2025-12-05 17:03:41 +0100

    En vez de hacer una clase cifras, he hecho una función cifras siguiendo el esquema de las diapositivas. Dos problemas (no sé si graves). 1-La primera operación siempre es 0+algo (porque se le pasa una solución con valor 0). 2-Solo se puede llegar a resultados acumulando. Si S={1,2,3,4} y objetivo=15, no puedo hacer 1+2, 3*4, 3+12

--- estudiante/estudiante/src/cifras.cpp
+++ estudiante/estudiante/src/cifras.cpp
@@ -7,7 +7,7 @@
 #include <time.h>
 
 using namespace std;
- 
+/* 
 class Cifras{  
     private:
         const vector<int> C = {1,2,3,4,5,6,7,8,9, 10, 25, 50, 75, 100};
@@ -39,7 +39,82 @@ class Cifras{
  
 
 
+};*/
+
+
+struct operaciones{
+   string op;
+   int cantidad;
 };
+
+struct solucion{
+   int valor;
+   operaciones ops;
+};
+
+vector<solucion> GeneraOperaciones(solucion actual, int n){
+   vector<solucion> v;
+      solucion suma;
+      suma.valor = actual.valor+n;
+      suma.ops.op = actual.ops.op+" "+to_string(actual.valor)+"+"+to_string(n);
+      suma.ops.cantidad = actual.ops.cantidad+1;
+      v.push_back(suma);
+   
+      solucion resta;
+      resta.valor = actual.valor-n;
+      if(resta.valor >= 0){
+         resta.ops.op = actual.ops.op+" "+to_string(actual.valor)+"-"+to_string(n);
+         resta.ops.cantidad = actual.ops.cantidad+1;
+         v.push_back(resta);      
+      }
+   
+      solucion multiplicacion;
+      multiplicacion.valor = actual.valor*n;
+      multiplicacion.ops.op = actual.ops.op+" "+to_string(actual.valor)+"*"+to_string(n);
+      multiplicacion.ops.cantidad = actual.ops.cantidad+1;
+      v.push_back(multiplicacion);
+   
+      solucion division;
+   
+      if((actual.valor==0 || actual.valor%n == 0) && n != 0){
+         division.valor = actual.valor/n;
+         division.ops.op = actual.ops.op+" "+to_string(actual.valor)+"/"+to_string(n);
+         division.ops.cantidad = actual.ops.cantidad+1;
+         v.push_back(division);      
+      }
+   
+   
+   
+   return v;
+}
+
+solucion Cifras(multiset<int> S, int objetivo, solucion actual, solucion best){
+   multiset<int> restantes;
+   vector<solucion> operaciones;
+   if(actual.valor == objetivo && best.valor != objetivo){
+      best = actual;
+      return best;
+   }
+   int diferenciaActual = abs(actual.valor-objetivo);
+   int diferenciaBest = abs(best.valor-objetivo);
+   if(diferenciaActual < diferenciaBest || (diferenciaActual==diferenciaBest && actual.ops.cantidad < best.ops.cantidad )){
+      best = actual;
+   }
+   for (auto it = S.begin(); it != S.end() ; it++){
+      restantes = S;
+      auto it2 = restantes.find(*it);
+      auto it3 = it2;
+      it3++;
+      restantes.erase(it2,it3);
+      operaciones = GeneraOperaciones(actual,*it);
+      for(auto it_aux = operaciones.begin(); it_aux != operaciones.end(); ++it_aux){
+         //cout << (*it_aux).ops.op << "\n";
+         best = Cifras(restantes, objetivo, *it_aux, best);
+         //cout << best.valor << ", " << best.ops.op << "\n";
+      }
+   }
+   return best;
+}
 
 
 
