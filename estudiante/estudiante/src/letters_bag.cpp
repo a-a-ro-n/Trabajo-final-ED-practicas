5db6626cf2abcce46c66967af8db70249e0c69c2
a-a-ro-n <aaronlopmol@gmail.com>
2025-12-09 16:02:07 +0100

    correcciones para el funcionamiento de letter_bag

--- estudiante/estudiante/src/letters_bag.cpp
+++ estudiante/estudiante/src/letters_bag.cpp
@@ -1,9 +1,9 @@
 #include "letters_bag.h"
 
-LettersBag::LettersBag(const LettersSet & conjunto){    // pasamos el numero de letras, y el conjunto dnde estan las letras
+LettersBag::LettersBag(const LetterSet & conjunto){    // pasamos el numero de letras, y el conjunto dnde estan las letras
     auto it = conjunto.begin();         // iterador a la primera posicon del conjunto
     
-    for(it; it != conjunto.end(); it++){
+    for(it; it != conjunto.end(); ++it){
         int reps = it->second.repetitions;  // repeticiones
         char letra = it->first;             // letra
 
@@ -12,39 +12,58 @@ LettersBag::LettersBag(const LettersSet & conjunto){    // pasamos el numero de 
     }
 }
 
-const char LettersBag::getLetter(int index) const {     // conseguimos la letra
+const char LettersBag::getLetter(int index) const { // conseguimos la letra
     char result = (index > -1 && index < bag.size()) ? bag[index] : '\0';
     return result;
 }
 
 // void setLetter(const char);          // añadimos la letra a la bolsa   (no le veo mucho sentido que podamos añadir letras)
 
-const string LettersBag::toString() {   // mostramos por pantalla todas las letras de la bolsa
+const string LettersBag::toString() const { // mostramos por pantalla todas las letras de la bolsa
     string result = "{";
-    for(int i = 0; i < bag.size()-1; i++)
-       result +=  bag[i] + ",";
-    result += bag[bag.size()-1] + "}";
+    
+    if(!bag.empty()){
+        for(int i = 0; i < bag.size(); ++i){
+            result +=  bag[i];
+            if(i < bag.size()-1)
+                result += ",";
+        }
+    }
+    
+    result += "}";
 
     return result;
 }
 
-LettersBag::iterator::iterator(){
-    /*
-        vector<char>::iterator aux;
-        it = aux;
-    */
+unsigned LettersBag::size() const {
+    return bag.size();
 }
 
-LettersBag::iterator::iterator(vector<char>::iterator & iter){
-    it(iter);
+bool LettersBag::empty() const {
+    return bag.empty();
 }
 
-char & LettersBag::iterator::operator*() const {
+void LettersBag::clear() {
+    bag.clear();
+}
+
+void LettersBag::erase(const char c) {
+    auto it = find(bag.begin(), bag.end(), c); // buscamos c en el conjunto
+    
+    if (it != bag.end()) // si esta, la eliminamos
+        bag.erase(it);
+}
+
+LettersBag::iterator::iterator(){}
+
+LettersBag::iterator::iterator(vector<char>::iterator iter) : it(iter){}
+
+char & LettersBag::iterator::operator*(){
     return *it;
 }
 
-iterator & LettersBag::iterator::operator++(){
-    ++it:
+LettersBag::iterator & LettersBag::iterator::operator++(){
+    ++it;
     return *this;
 }
 
@@ -56,10 +75,10 @@ bool LettersBag::iterator::operator==(const iterator & other) const{
     return  it == other.it;
 }
 
-LettersBag::iterator begin(){
+LettersBag::iterator LettersBag::begin(){
     return iterator(bag.begin());
 }
 
-LettersBag::iterator end(){
+LettersBag::iterator LettersBag::end(){
     return iterator(bag.end());
 }
