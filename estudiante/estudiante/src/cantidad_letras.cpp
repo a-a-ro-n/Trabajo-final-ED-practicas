1911cbb79ab6a0eaa295891b17a0e8c335e46207
a-a-ro-n <aaronlopmol@gmail.com>
2025-12-10 12:03:47 +0100

    empezando

--- estudiante/estudiante/src/cantidad_letras.cpp
+++ estudiante/estudiante/src/cantidad_letras.cpp
@@ -8,14 +8,21 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  if(argc != 4){
-    cout << "Los parametros son: " << endl;
-    cout << "1.- El fichero con el diccionario" << endl;
-    cout << "2.- El fichero con las letras" << endl;
-    cout << "3.- El fichero de salida"<<endl;
-    return 0;
-  }
-  //A completar
+    if(argc != 4){
+      cout << "Los parametros son: " << endl;
+      cout << "1.- El fichero con el diccionario" << endl;
+      cout << "2.- El fichero con las letras" << endl;
+      cout << "3.- El fichero de salida"<<endl;
+      return 0;
+    }
 
+    ifstram dic(argv[1]);
+    Dictionary diccionario;
 
+    if(dic){ // Si se abre el archivo porporcionado, lo registramos como diccionario
+        dic >> diccionario; 
+        dic.close();
+    }
+    
+    
 }
