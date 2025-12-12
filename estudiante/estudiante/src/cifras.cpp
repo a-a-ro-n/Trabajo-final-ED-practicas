cc4ab4c8e0b9bc2cf0136a34c12b21dbe3a3dbec
pablo <pablo@FamiliaBM>
2025-12-12 15:09:00 +0100

    he arreglado el main y he hecho el doxyfile de cifras

--- estudiante/estudiante/src/cifras.cpp
+++ estudiante/estudiante/src/cifras.cpp
@@ -8,40 +8,54 @@
 
 using namespace std;
 
-// CREO QUE ESTO SE PUDE HACER CON UN MAP<INT,STRING> 
-struct operaciones{
-   string op;
-   int cantidad;
-};
+/**
+  *@brief Cifras
+  *Utilizamos dos funciones y un struct para calcular las operaciones necesarias para llegar a un número aleatorio de 3 cifras generado en el main, utilizando los números de un que se dan
+  *
+*/
 
+
+/**
+  *@brief struct solucion
+  *Guarda una solución, guardando el valor al que se ha llegado, un string con las operaciones utilizadas para mostrarlas por pantalla y la cantidad de operaciones que han sido necesarias
+*/
 struct solucion{
    int valor;
-   operaciones ops;
+    string op;
+    int cantidad;
 };
+
+/**
+  *@brief Calcula las posibles operaciones que se pueden conseguir, dadas una solución actual y un número que se le debe sumar, restar, multiplicar o dividir
+  *
+  *@param actual es la solución parcial
+  *@param n es el número que operamos con actual
+  *@return un vector de soluciones. Cada elemento será actual más/menos/por/entre n, según las operaciones que se puedan hacer (p.e. no puede haber números negativos)
+*/
 
 vector<solucion> GeneraOperaciones(solucion actual, int n){
    vector<solucion> v;
       solucion suma;
       suma.valor = actual.valor+n;
       if(actual.valor != 0){
-        suma.ops.op = actual.ops.op+" "+to_string(actual.valor)+"+"+to_string(n);
-        suma.ops.cantidad = actual.ops.cantidad+1;
+        suma.op = actual.op+" "+to_string(actual.valor)+"+"+to_string(n);
+        suma.cantidad = actual.cantidad+1;
       }
       v.push_back(suma);
    
       solucion resta;
       resta.valor = actual.valor-n;
       if(resta.valor >= 0){
-         resta.ops.op = actual.ops.op+" "+to_string(actual.valor)+"-"+to_string(n);
-         resta.ops.cantidad = actual.ops.cantidad+1;
+         resta.op = actual.op+" "+to_string(actual.valor)+"-"+to_string(n);
+         resta.cantidad = actual.cantidad+1;
          v.push_back(resta);      
       }
    
       solucion multiplicacion;
       multiplicacion.valor = actual.valor*n;
       if(actual.valor != 0){
-        multiplicacion.ops.op = actual.ops.op+" "+to_string(actual.valor)+"*"+to_string(n);
-        multiplicacion.ops.cantidad = actual.ops.cantidad+1;
+        multiplicacion.op = actual.op+" "+to_string(actual.valor)+"*"+to_string(n);
+        multiplicacion.cantidad = actual.cantidad+1;
       }
       v.push_back(multiplicacion);
    
@@ -49,8 +63,8 @@ vector<solucion> GeneraOperaciones(solucion actual, int n){
    
       if((actual.valor==0 || actual.valor%n == 0) && n != 0){
          division.valor = actual.valor/n;
-         division.ops.op = actual.ops.op+" "+to_string(actual.valor)+"/"+to_string(n);
-         division.ops.cantidad = actual.ops.cantidad+1;
+         division.op = actual.op+" "+to_string(actual.valor)+"/"+to_string(n);
+         division.cantidad = actual.cantidad+1;
          v.push_back(division);      
       }
    
@@ -58,6 +72,15 @@ vector<solucion> GeneraOperaciones(solucion actual, int n){
    
    return v;
 }
+
+/**
+  *@brief Calcula la mejor solución para llegar al objetivo con los números del conjunto S
+  *@param S es el conjunto de números con los que hay que operar para obtener la solución
+  *@param objetivo es el número que hay que alcanzar o acercarse al máximo
+  *@param actual es la solucion actual, sobre la que seguiremos operando para llegar a la mejor
+  *@param best es la mejor solución hasta el momento
+  *@return devuelve una solucion con el valor exacto con el mínimo número de operaciones o la solución más cercana (también con el mínimo de operaciones)
+*/
 
 solucion Cifras(multiset<int> S, int objetivo, solucion actual, solucion best){
    multiset<int> restantes;
@@ -68,7 +91,7 @@ solucion Cifras(multiset<int> S, int objetivo, solucion actual, solucion best){
    }
    int diferenciaActual = abs(actual.valor-objetivo);
    int diferenciaBest = abs(best.valor-objetivo);
-   if(diferenciaActual < diferenciaBest || (diferenciaActual==diferenciaBest && actual.ops.cantidad < best.ops.cantidad )){
+   if(diferenciaActual < diferenciaBest || (diferenciaActual==diferenciaBest && /*actual.ops.cantidad < best.ops.cantidad*/ actual.cantidad < best.cantidad)){
       best = actual;
    }
    for (auto it = S.begin(); it != S.end() ; it++){
@@ -79,13 +102,17 @@ solucion Cifras(multiset<int> S, int objetivo, solucion actual, solucion best){
       restantes.erase(it2,it3);
       operaciones = GeneraOperaciones(actual,*it);
       for(auto it_aux = operaciones.begin(); it_aux != operaciones.end(); ++it_aux){
-         //cout << (*it_aux).ops.op << "\n";
          best = Cifras(restantes, objetivo, *it_aux, best);
-         //cout << best.valor << ", " << best.ops.op << "\n";
       }
    }
    return best;
 }
+
+/**
+  *@Genera un multiset S de 6 elementos, escogidos al azar del conjunto C. Genera un número aleatorio de tres cifras al que hay que llegar. Pregunta al usuario su solución.
+  * Esta solución no tiene usa paréntesis, hace las operaciones de izquierda a derecha hasta que hay un valor inválido (al usuario se le recomienda usar ..). 
+  *Después calcula con Cifras, la mejor solución y la muestra por pantalla
+*/
 
 int main(){
    const vector<int> C = {1,2,3,4,5,6,7,8,9, 10, 25, 50, 75, 100};
@@ -94,16 +121,16 @@ int main(){
    srand(time(NULL));
    int min = 100;
    int max = 1000;
-   //genero nÃºmero aleatorio de 3 cifras
+   //genero número aleatorio de 3 cifras
    objetivo = (rand()%(max-min+1))+min;
-   //cojo 6 nÃºmeros aleatorios de C
+   //cojo 6 números aleatorios de C
    min = 0;
    max = 13;
    for(int i = 0; i < 6; i++){
       S.insert(C[(rand()%(max+1))]);
    }
    
-   solucion actual = {0,""};
+   solucion actual = {0,"",0};
    solucion best = actual;
    
    cout << "El conjunto es " << endl;
@@ -118,33 +145,41 @@ int main(){
    int acumulador=0;
    char operador;
    bool valido = true;
+    bool primero_valido = true;
    string tu_solucion;
-   cout << "Dime tu solución" << endl;
+   cout << "Dime tu solución. Usa '..' para terminar " << endl;
    cin >> acumulador;
-   do{
-    cin >> operador;    
-    cin >> num;
-    if((operador!='+' && operador != '-' && operador != '/' && operador != '*') || (S.find(num) == S.end()) || num < 0 || (operador=='-' && acumulador<=num) || (operador=='/' && num ==0) ){
+   if((S.find(acumulador) == S.end())){
         valido = false;
+        primero_valido = false;
+        cout << "Primer elemento erróneo" << endl;
     } else{
-        tu_solucion += "\t"+to_string(acumulador)+operador+to_string(num);
-        if(operador == '+'){
-            acumulador += num;
-        } else if (operador == '-'){
-            acumulador -= num;
-        } else if (operador == '*'){
-            acumulador *= num;
-        } else{
-            acumulador /= num;
+        S.erase(acumulador);
+        while(valido){
+            cin >> operador;    
+            cin >> num;
+            if((operador!='+' && operador != '-' && operador != '/' && operador != '*') || (S.find(num) == S.end()) || num < 0 || (operador=='-' && acumulador<=num) || (operador=='/' && num ==0) ){
+                valido = false;
+            } else{
+                tu_solucion += "\t"+to_string(acumulador)+operador+to_string(num);
+                S.erase(num);
+                if(operador == '+'){
+                    acumulador += num;
+                } else if (operador == '-'){
+                    acumulador -= num;
+                } else if (operador == '*'){
+                    acumulador *= num;
+                } else{
+                    acumulador /= num;
+                }
+            }
         }
+
+        cout << "Has llegado al valor: " << acumulador << "mediante las siguientes operaciones: " << tu_solucion << endl;
     }
-   }while(valido);
-    
-    cout << "Has llegado al valor: " << acumulador << "mediante las siguientes operaciones: " << tu_solucion << endl;
+   
 
-
-
-   cout << "La mejor solución es " << salida.valor << ". Para ello, hemos realizado las siguientes operaciones: " << salida.ops.op << endl;
+   cout << "La mejor solución es " << salida.valor << ". Para ello, hemos realizado las siguientes operaciones: " <<  salida.op << endl;
 
 
 }
