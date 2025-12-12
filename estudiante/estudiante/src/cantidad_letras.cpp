606c8f5dece4e3853a5c333a854c09c5b5ab88ea
pablo <pablo@FamiliaBM>
2025-12-12 19:00:39 +0100

    terminado cantidad_letras

--- estudiante/estudiante/src/cantidad_letras.cpp
+++ estudiante/estudiante/src/cantidad_letras.cpp
@@ -3,8 +3,16 @@
 #include <cmath>
 #include "dictionary.h"
 #include "letters_set.h"
+#include <limits>
+#include <vector>
 
 using namespace std;
+
+struct letra{
+    char caracter;
+    int apariciones;
+    double porcentaje;
+};
 
 int main(int argc, char *argv[])
 {
@@ -16,8 +24,9 @@ int main(int argc, char *argv[])
       return 0;
     }
 
-    ifstram dic(argv[1]);
     Dictionary diccionario;
+    ifstream dic(argv[1]);
+    
 
     if(dic){ // Si se abre el archivo porporcionado, lo registramos como diccionario
         dic >> diccionario; 
@@ -28,7 +37,38 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    ofstream salida(argv[3]);
+    if(!salida){
+        cerr << "Error al abrir el archivo de salida: " << argv[3] << endl;
+        return 1;
+    }
+
     LetterSet conjunto(argv[2]); // registramos el conjunto de letras
+
+    double min = numeric_limits<double>::infinity();
+
+    int total_letras = diccionario.getTotalLetters();
+    vector<letra> v(conjunto.size());
+    int apariciones;
+    double porcentaje;
+    double min_log;
+    int i = 0;
+    for(auto it = conjunto.begin(); it != conjunto.end(); ++it){
+        v[i].caracter = it->first;
+        apariciones = diccionario.getOccurrences(it->first);
+        v[i].apariciones = apariciones;
+        porcentaje = (double)apariciones/total_letras;
+        v[i].porcentaje = porcentaje;
+        if(min > porcentaje){
+            min = porcentaje;
+        }
+        ++i;
+    }
+    min_log = -log10(min);
+    salida << "#Letra\tCantidad\tPuntos" << endl;
+    for(auto it = v.begin(); it != v.end(); ++it){
+        salida << it->caracter << "\t" << it->apariciones << "\t" << (int)(10*(-log10(it->porcentaje)/min_log)) << endl;
+    }
     
-       
+    salida.close();
 }
